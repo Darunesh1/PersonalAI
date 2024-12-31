656efc5e54ec0cbba7d42280711bc5371aff1ce0
Darunesh1 <sathyarajasekar5873@gmail.com>
2024-12-31 18:00:18 +0530

    arranges everything

--- conv.py
+++ conv.py
@@ -1,17 +1,18 @@
-random_text=[
-    "hello hi there",
-    "how are you",
-    "what's up",
-    "fuck you",
-    "your luck sucks",
-    "bye",
-    "see you later",
-]
+import os
 
-farewell_text=[
-    "Goodbye!",
-"See you later!",
-"Take care!",
-"Have a great day!",
-"Talk to you soon!",
-]
+EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
+
+NEWS = os.environ.get("NEWS")
+WEATHER = os.environ.get("WEATHER")
+APP_ID = os.environ.get("APP_ID")
+
+IP_URL='https://api.ipify.org?format=json'
+
+NEWS_URL="https://newsdata.io/api/1/latest"
+
+WEATHER_URL="https://api.openweathermap.org/data/2.5/weather"
+
+SMTP_URL="smtp.gmail.com"
+SMTP_PORT=587
+
